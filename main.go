1c1c4d71c41ea25c65f5b9b19b38215fb3b5c12c
Artun Colak <artuncolak97@gmail.com>
2024-08-24 01:15:19 +0300

    feat: add tefas client

--- main.go
+++ main.go
@@ -1,9 +1,101 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+type TefasClient struct {
+	baseUrl *url.URL
+	client  *http.Client
+}
+
+const (
+	baseUrl    = "https://www.tefas.gov.tr"
+	referer    = "http://www.tefas.gov.tr/TarihselVeriler.aspx"
+	dateFormat = "02-01-2006"
+)
+
+/*
+New creates a new instance of TefasClient.
+*/
+func New() *TefasClient {
+	parsedURL, _ := url.Parse(baseUrl)
+
+	return &TefasClient{
+		baseUrl: parsedURL,
+		client:  &http.Client{},
+	}
+}
+
+func (c *TefasClient) request(method, path string, urlParams url.Values) (*http.Response, error) {
+	relPath, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	finalUrl := c.baseUrl.ResolveReference(relPath)
+
+	bodyReader := strings.NewReader(urlParams.Encode())
+	req, err := http.NewRequest(method, finalUrl.String(), bodyReader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+	req.Header.Add("Origin", baseUrl)
+	req.Header.Add("Referer", referer)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept", "application/json")
+
+	return c.client.Do(req)
+}
+
+func (c *TefasClient) unmarshalResponseBody(body io.Reader, v interface{}) error {
+	bodyBytes, err := io.ReadAll(body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bodyBytes, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+GetFundInfo retrieves fund information based on the provided FundInfoRequest.
+
+It sends a POST request to the "/api/DB/BindHistoryInfo" endpoint with the specified parameters.
+
+The function returns a slice of Fund objects and an error if any occurred during the request or response handling.
+*/
+func (c *TefasClient) GetFundInfo(fundInfoRequest FundInfoRequest) ([]Fund, error) {
+	params := url.Values{}
+	params.Add("fontip", string(fundInfoRequest.Type))
+	params.Add("fonkod", strings.ToUpper(fundInfoRequest.Code))
+	params.Add("bastarih", fundInfoRequest.StartDate.Format(dateFormat))
+	params.Add("bittarih", fundInfoRequest.EndDate.Format(dateFormat))
+
+	resp, err := c.request("POST", "/api/DB/BindHistoryInfo", params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var fundInfoResponse FundInfoResponse
+	err = c.unmarshalResponseBody(resp.Body, &fundInfoResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fundInfoResponse.Data, nil
 }
---
+++ types.go
@@ -0,0 +1,36 @@
+package main
+
+import "time"
+
+type Fund struct {
+	Date            string  `json:"TARIH"`
+	Code            string  `json:"FONKODU"`
+	Name            string  `json:"FONUNVAN"`
+	Price           float64 `json:"FIYAT"`
+	UnitCount       float64 `json:"TEDPAYSAYISI"`
+	InvestorCount   float64 `json:"KISISAYISI"`
+	PortfolioAmount float64 `json:"PORTFOYBUYUKLUK"`
+	ExchangePrice   string  `json:"BORSABULTENFIYAT"`
+}
+
+type FundType string
+
+const (
+	YAT FundType = "YAT"
+	EMK FundType = "EMK"
+	BYF FundType = "BYF"
+)
+
+type FundInfoRequest struct {
+	Type      FundType
+	Code      string
+	StartDate time.Time
+	EndDate   time.Time
+}
+
+type FundInfoResponse struct {
+	Draw            int    `json:"draw"`
+	RecordsTotal    int    `json:"recordsTotal"`
+	RecordsFiltered int    `json:"recordsFiltered"`
+	Data            []Fund `json:"data"`
+}
